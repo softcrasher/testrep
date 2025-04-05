48460b25bcab75f9ca0f21e4954cb07dc57e9691
OK <neilanpadilla5@gmail.com>
2025-04-05 14:14:10 +0800

    Add files via upload

---
+++ loginpage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log-in page</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <h1>log-in page</h1>
+    <form action="verified.php" method="post">
+      <label for="email">email: </label><br>
+      <input type="text" name="email" placeholder="example@gmail.com"><br>
+      <label for="pass">password: </label><br>
+      <input type="password" name="pass"><br>
+      <input type="submit" value="log-in" class="submit">
+    </form>
+  </body>
+</html>
